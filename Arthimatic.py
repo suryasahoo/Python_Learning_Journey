5d357ea7e663e4b8dcfccae552f0d62e26f323d8
suryasahoo <suryasahoo-123@outlook.com>
2025-05-04 16:16:38 +0530

    arthimatic operaton done

---
+++ Arthimatic.py
@@ -0,0 +1,49 @@
+a,b = 5,3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#divisional numbers
+
+#float div
+
+a,b =10,3
+print(a/b)
+
+#integer div //
+
+print(a//b)
+
+#modulus remainder %
+
+print(a % b)
+
+#Exponentiation
+
+#power of number(**)
+
+base , exponent = 2,5
+print(base ** exponent)
+
+#square root using exponent
+num= 16
+print(num ** 0.5)
+
+#COMPOUND EXPRESSION
+
+#calculate avg of 3 numbers
+
+a,b,c = 4,5,6
+average = (a+b+c)/3
+print(average)
+
+
+#compund interest formula
+
+principal =1000
+rate = 0.05
+time = 3
+amount = principal *(1+rate)** time
+print(amount)
+
