4402ac50bf633e52aeff40cbcf85d10879643757
suryasahoo <suryasahoo-123@outlook.com>
2025-05-05 11:40:49 +0530

    added comparsion operator

---
+++ comparsion.py
@@ -0,0 +1,19 @@
+
+
+
+#comparsion Operator
+
+x,y =5 ,7
+
+print(x == y) #false equal to
+
+print(x != y)  #True nt equal to
+
+print(x > y)  #False greater then
+
+print(x < y) #True less than
+
+print(x >= y) #False greater than equal to 
+
+print(x <= y) # True smaller than equal to
+
